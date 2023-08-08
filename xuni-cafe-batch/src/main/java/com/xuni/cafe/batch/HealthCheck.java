0c27ad1a51af39b7c55e14a73c06c4b63f71e824
JxxHxxx <leesin5498@naver.com>
2023-08-08 15:20:39 +0900

    Refactor/batch 프로젝트 의존성 추가 및 헬스 체커 (#11)

--- xuni-cafe-batch/src/main/java/com/xuni/cafe/batch/HealthCheck.java
+++ xuni-cafe-batch/src/main/java/com/xuni/cafe/batch/HealthCheck.java
@@ -1,5 +1,15 @@
 package com.xuni.cafe.batch;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class HealthCheck {
+
+    @GetMapping("/health-batch")
+    public ResponseEntity<String> checkBatchServerHealth() {
+
+        return ResponseEntity.status(200).body("batch-server-ok");
+    }
 }
